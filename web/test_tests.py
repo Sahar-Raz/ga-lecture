56be916c5d0887507c001e6bc6ecbf905eb40f3c
unknown <2690430@INET-ASP.GGNET.JP>
2026-07-14 13:27:34 +0900

    mistake

--- web/test_tests.py
+++ web/test_tests.py
@@ -6,4 +6,4 @@ class TestTestView(TestCase):
          url = reverse('test')
          response = self.client.get(url)
          self.assertEqual(response.status_code, 200)
-         self.assertContains(response, "This is a test vie.")
+         self.assertContains(response, "This is a test vi.")
